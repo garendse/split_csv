741c8c41cdbd8228eb9115907299338377f819ec
Garth Arendse <garth.arendse@gmail.com>
2022-10-02 23:25:46 +0200

    Initial

---
+++ main.py
@@ -0,0 +1,48 @@
+import PySimpleGUI as sg
+import pandas as pd
+
+gchunk_size=5000
+gprefix="part_"
+
+def _split_csv_file(csv_file: str, chunk_size: int) -> None:
+    try:
+        for i, chunk in enumerate(pd.read_csv(csv_file, sep='|', chunksize=chunk_size, header=1)):
+            print(f"Writing chunk {i}")
+            chunk.to_csv(gprefix + "-" + str(i) + ".csv", index=False, sep='|', na_rep="", header=True)
+
+    except pd.errors.EmptyDataError:
+        print("No data in file: " + csv_file)
+
+def _get_csv_filename() -> str:
+    global gprefix
+    global gchunk_size
+
+    layout = [
+        [sg.Text("CSV Data File: "), sg.Input(), sg.FileBrowse(key="-CSV-", file_types=(("CSV Files", "*.csv"),))],
+        [sg.Text("Chunk Prefix: "), sg.Input(key='-CKP-', size=(6, 1), default_text=f"{gprefix}")],
+        [sg.Text("Chunk Size: "), sg.Input(key='-CKN-', size=(6, 1), default_text=f"{gchunk_size}")],
+        [sg.Button("Submit"), sg.Cancel()]]
+
+    window = sg.Window("CSV File Splitter", layout)
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            break
+        if event == "Submit":
+            gchunk_size = int(values['-CKN-'])
+            gprefix = values['-CKP-']
+            return values["-CSV-"]
+
+
+if __name__ == '__main__':
+    csv_fname = _get_csv_filename()
+
+    if len(csv_fname) > 0:
+        try:
+            print("Reading", csv_fname)
+            _split_csv_file(csv_fname, gchunk_size)
+            print("Done")
+
+        except Exception as e:
+            print("Error:", e)
